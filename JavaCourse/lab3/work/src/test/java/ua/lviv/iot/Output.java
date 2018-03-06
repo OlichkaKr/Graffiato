1903f33311283f5b01af84db306cc916c1d6ce6c
Olga Kr <olichka121kr76@gmail.com>
2018-03-06 23:08:34 +0200

    Changes for lab6

---
+++ JavaCourse/lab3/work/src/test/java/ua/lviv/iot/Output.java
@@ -0,0 +1,29 @@
+package ua.lviv.iot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public abstract class Output {
+
+    protected ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+}
