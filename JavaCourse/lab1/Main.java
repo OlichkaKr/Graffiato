76cb4b605a4370e2454132ef94166ed8e644951a
Olga Kr <olichka121kr76@gmail.com>
2018-06-10 23:22:01 +0300

    Cleaning the code

--- JavaCourse/lab1/Main.java
+++ JavaCourse/lab1/Main.java
@@ -4,27 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Plain Gulfstream = new Plain();
-		Plain Boeing = new Plain("Boeing", 5, 14, 100);
-		Plain Airbus = new Plain("Airbus", 12, 47, 1000, 15);
+		Plain gulfstream = new Plain();
+		Plain boeing = new Plain("Boeing", 5, 14, 100);
+		Plain airbus = new Plain("Airbus", 12, 47, 1000, 15);
 		
-		System.out.println(Gulfstream.toString());
-		System.out.println(Boeing.toString());
-		System.out.println(Airbus.toString());
-		
-		Plain.printStaticSum();
-		Airbus.printSum();
-		
-		Gulfstream.resetValues("Gulfstream", 5, 5, 20, 10);
-		Airbus.resetValues("Airbus", 10, 30, 500, 10);
-		Boeing.setLoadCapacity(15);
-		
-		System.out.println(Gulfstream.toString());
-		System.out.println(Boeing.toString());
-		System.out.println(Airbus.toString());
+		System.out.println(gulfstream.toString());
+		System.out.println(boeing.toString());
+		System.out.println(airbus.toString());
 		
 		Plain.printStaticSum();
-		Gulfstream.printSum();
+		airbus.printSum();
+		
+		gulfstream.resetValues("Gulfstream", 5, 5, 20, 10);
+		airbus.resetValues("Airbus", 10, 30, 500, 10);
+		boeing.setLoadCapacity(15);
+		
+		System.out.println(gulfstream.toString());
+		System.out.println(boeing.toString());
+		System.out.println(airbus.toString());
+		
+		Plain.printStaticSum();
+		gulfstream.printSum();
 
 	}
 
